ac37c931602b6d214ac77af841144bd67d4dd933
baubrun <baubelf@gmail.com>
2021-08-20 06:35:01 -0400

    removed get order

--- controllers/order.js
+++ controllers/order.js
@@ -3,30 +3,7 @@ import ValidateDTO from "../dto/order.js";
 import winston from "winston";
 
 /**
- * GET order
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- */
-export const getOrder = async (req, res) => {
-  const params = req.query;
-
-  const { error } = ValidateDTO.postOrder(params);
-  if (error) return res.status(400).send(error.details[0]?.message);
-
-  try {
-    let order = await Order.findOne({ product_id: params });
-
-    if (!order) return res.status(400).send("Order not found");
-
-    return res.send(order);
-  } catch (err) {
-    winston.error("getOrder", err);
-    res.status(400).send(err?.message);
-  }
-};
-
-/**
- * Add order
+ * POST order
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  */
